24a198d241ebc6e53cc8f95319a9f2fcd18a064c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-12 22:32:55 -0600

    Add attr_reader methods and tests

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -6,7 +6,13 @@ class DishTest < Minitest::Test
   def setup
     @dish = Dish.new("Couscous Salad", :appetizer)
   end
+
   def test_it_exists
     assert_instance_of Dish, @dish
+  end
+
+  def test_it_has_a_name_and_a_category
+    assert_equal "Couscous Salad", @dish.name
+    assert_equal :appetizer, @dish.category
   end
 end
