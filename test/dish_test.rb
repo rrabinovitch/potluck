1f870be0c6b4f64fd5c7fc9982e6b0f9ea5aa5d9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-12 22:28:24 -0600

    Create dish classs and setup test; complete iteration 1

---
+++ test/dish_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test
+  def setup
+    @dish = Dish.new("Couscous Salad", :appetizer)
+  end
+  def test_it_exists
+    assert_instance_of Dish, @dish
+  end
+end
